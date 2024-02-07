9674ace00c82365c06f191b8d31e42f480e2a4c5
Brent Pappas <pappasbrent@gmail.com>
2024-02-06 21:03:47 -0500

    Remove comments and includes and add comments
    - Remove an unused include
    - Remove commented out code
    - Add code explaining what certain callbacks do

--- lib/PluginASTAction.cc
+++ lib/PluginASTAction.cc
@@ -3,17 +3,9 @@
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "llvm/ADT/StringRef.h"
-#include <llvm-17/llvm/Support/raw_ostream.h>
 #include <memory>
 
 namespace c_taint {
-
-// class ASTConsumer : public clang::ASTConsumer {
-//     public:
-//         ASTConsumer(clang::CompilerInstance &CI) {
-//                 llvm::outs() << "Hello, world!\n";
-//         }
-// };
 
 class PluginASTAction : public clang::PluginASTAction {
     public:
@@ -26,22 +18,30 @@ class PluginASTAction : public clang::PluginASTAction {
         PluginASTAction &operator=(PluginASTAction const &) = delete;
 
     private:
+        // Hook for defining custom command line argument parsing for this
+        // plugin.
         bool ParseArgs(clang::CompilerInstance const &,
                        std::vector<std::string> const &Args) override {
                 return true;
         }
 
+        // Create the plugin's AST consumer so that it can do its thing.
         std::unique_ptr<clang::ASTConsumer>
         CreateASTConsumer(clang::CompilerInstance &CI,
                           llvm::StringRef InFile) override {
                 return std::make_unique<ASTConsumer>(CI);
         }
 
+        // We use this to tell Clang when we want to perform our action. In this
+        // case, we perform our action before the main action so that we can run
+        // our taint analysis even if we don't compile the target translation
+        // unit (e.g. with the command line option -fsyntax-only).
         clang::PluginASTAction::ActionType getActionType() override {
                 return clang::PluginASTAction::ActionType::AddBeforeMainAction;
         }
 };
 
+// Register the plugin with clang.
 static clang::FrontendPluginRegistry::Add<PluginASTAction>
         X("c_taint_analyzer", "C89 taint analyzer");
 }
