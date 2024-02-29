85c50d76b1631e3079ed5beac66940d4d87ebbcc
rorovas <ro073916@ucf.edu>
2024-02-28 20:08:26 -0500

    three more tests

---
+++ test/assgn_taint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void sanitize(int *x){
+    if(*x != 0){
+        *x = 0;
+    }
+}
+
+int main(void){
+    int x;
+    int y;
+    scanf("%d", &x);
+    
+    y = x;
+    y = 5;
+    return 0;
+}
